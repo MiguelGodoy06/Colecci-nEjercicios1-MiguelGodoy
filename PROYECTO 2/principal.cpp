8490e281e3ac9f14854ef532ca8331e4f6e59e85
MiguelGodoy06 <miguelgodoyavila1@gmail.com>
2024-10-31 12:52:34 +0100

    Proyecto 2

---
+++ PROYECTO 2/principal.cpp
@@ -0,0 +1,8 @@
+#include "principal.h"
+#include <iostream>
+using namespace std;
+
+int principal() {
+    cout<<"Hola Clion"<<endl;
+    return 0;
+}
---
+++ PROYECTO 2/principal.h
@@ -0,0 +1,16 @@
+//
+// Created by Miguel Godoy on 31/10/24.
+//
+
+#ifndef PRINCIPAL_H
+#define PRINCIPAL_H
+
+
+
+class principal {
+
+};
+
+
+
+#endif //PRINCIPAL_H
